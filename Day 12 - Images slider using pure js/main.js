4e600031298bfeb00b068b72dee3c4bf6aa4d791
Gustavo <gustavo.4lv5s@gmail.com>
2020-07-01 23:02:23 -0300

    day 12 finished

---
+++ Day 12 - Images slider using pure js/main.js
@@ -0,0 +1,21 @@
+let time = 1000,
+  currentImageIndex = 0,
+  images = document.querySelectorAll("img"),
+  max = images.length - 1;
+
+function nextImage() {
+  images[currentImageIndex].classList.remove("selected");
+
+  if (currentImageIndex === max) 
+    currentImageIndex = 0;
+  else currentImageIndex++;
+
+  images[currentImageIndex].classList.add("selected");
+}
+
+(function start() {
+  console.log("Tes");
+  setInterval(() => {
+    nextImage();
+  }, time);
+})();
