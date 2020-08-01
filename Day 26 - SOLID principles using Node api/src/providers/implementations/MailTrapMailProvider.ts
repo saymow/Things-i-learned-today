a304452e2006ddbeb11292c9c11e76f703232f08
Gustavo <gustavo.4lv5s@gmail.com>
2020-08-01 19:20:26 -0300

    day 26 hidden keys

--- Day 26 - SOLID principles using Node api/src/providers/implementations/MailTrapMailProvider.ts
+++ Day 26 - SOLID principles using Node api/src/providers/implementations/MailTrapMailProvider.ts
@@ -1,17 +1,20 @@
 import { IMailProvider, IMessage } from "../IMailProvider";
 import nodemailer from "nodemailer";
 import Mail from "nodemailer/lib/mailer";
+import dotenv from "dotenv";
+dotenv.config();
 
 export class MailTrapMailProvider implements IMailProvider {
   private transporter: Mail;
 
   constructor() {
+    console.log(process.env);
     this.transporter = nodemailer.createTransport({
       host: "smtp.mailtrap.io",
       port: 2525,
       auth: {
-        user: "491c61ab374388",
-        pass: "a8ffaa22c57e19",
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
       },
     });
   }
